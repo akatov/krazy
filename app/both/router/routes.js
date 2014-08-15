55cec61ffbe98ae890538d723bede94c0ca09c85
Dmitri Akatov <akatov@gmail.com>
2014-08-15 12:25:27 +0100

    have to be signed in for boards.show route

--- app/both/router/routes.js
+++ app/both/router/routes.js
@@ -9,6 +9,12 @@ Router.configure({
   routeControllerNameConverter: 'upperCamelCase'
 });
 
+Router.onBeforeAction(function(){
+  if (!Meteor.userId()) {
+    Router.go('index')
+  }
+}, {only: ['boards.show']})
+
 Router.map(function () {
   this.route('index', {path: '/'});
   this.route('boards.show', {path: '/boards/:_id'});
